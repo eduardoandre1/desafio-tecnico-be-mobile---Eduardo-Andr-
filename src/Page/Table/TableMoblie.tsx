0e489a2d8038610a8dd02afe1a3db10ebe837e5e
Eduardo André <eduardo_andre@id.uff.br>
2024-02-29 22:23:51 -0300

    feat: tableMobile table header

--- src/Page/Table/TableMoblie.tsx
+++ src/Page/Table/TableMoblie.tsx
@@ -3,21 +3,18 @@ import styled from 'styled-components';
 export default function TableMobile({ data }) {
   return (
     <div>
-      <div> menu</div>
-      <DetailsFormat>
-        <summary>
-          <img src="https://yt3.ggpht.com/ytc/AIdro_kiskoEuUllc8rEuH8Z1P7T0TXw7qACho7SyQALmg=s176-c-k-c0x00ffffff-no-rj" />
-          <h1>nome</h1>
-        </summary>
-        <li>data</li>
-        <li>cargo</li>
-      </DetailsFormat>
+      <TableHeader>
+        <h1>FOTO</h1>
+        <h1>NOME</h1>
+        <h1>.</h1>
+      </TableHeader>
       {data.map((data) => {
         return (
           <DetailsFormat>
             <summary>
               <img src={data.image} />
               <h1>{data.name}</h1>
+              <h1>icone</h1>
             </summary>
             <ul>
               <li>{data.admission_date}</li>
@@ -44,7 +41,11 @@ const DetailsFormat = styled.details`
   summary {
     background-color: beige;
     display: flex;
-    flex-direction: row;
+flex-direction: row;
+justify-content: space-between;
+align-items: center;
+padding: 14px 0px;
+gap: 32px;
     img {
       height: 34px;
       width: 34px;
@@ -55,3 +56,23 @@ const DetailsFormat = styled.details`
     background-color: orange;
   }
 `;
+const TableHeader = styled.div`
+display: flex;
+flex-direction: row;
+justify-content: space-between;
+align-items: center;
+padding: 14px 0px;
+gap: 32px;
+
+height: 47px;
+
+background: linear-gradient(180deg, #5A84C0 0%, #594ED2 100%);
+box-shadow: 0px 1px 2px rgba(0, 0, 0, 0.2);
+border-radius: 8px 8px 0px 0px;
+margin: 0 auto;
+color: #FFFFFF;
+
+h1 {
+  margin: 0 auto;
+}
+`

8fa2d4c4954c2a09e3030f5ff94df25f1b92fe02
Eduardo André <eduardo_andre@id.uff.br>
2024-03-03 21:16:12 -0300

    feat: sweetalter2

--- src/components/defautsFunctions/employers/employersFilter.tsx
+++ src/components/defautsFunctions/employers/employersFilter.tsx
@@ -1,13 +1,22 @@
+import Swal from 'sweetalert2';
 export default function employersFilter(data, searchName) {
-    if(searchName.length === 0) {
-      return data;
-    }
-    const filter = data.filter((enployer) => {
-    if(enployer.name.toLowerCase().includes(searchName.toLowerCase())||enployer.job.toLowerCase().includes(searchName.toLowerCase()))
-    return enployer;
+  if (searchName.length === 0) {
+    return data;
+  }
+  const filter = data.filter((enployer) => {
+    if (
+      enployer.name.toLowerCase().includes(searchName.toLowerCase()) ||
+      enployer.job.toLowerCase().includes(searchName.toLowerCase())
+    )
+      return enployer;
   });
-  if(filter.length === 0 && searchName.length > 0){
-    alert('usuario ou cargo não encontrados')
+  if (filter.length === 0 && searchName.length > 0) {
+    Swal.fire({
+      title: 'Não foi possivel encontrar',
+      text: 'verifique se o usuario usuario ou o cargo foram escritos corretamente',
+      icon: 'error',
+      confirmButtonText: 'ok',
+    });
     return data;
   }
   return filter;
